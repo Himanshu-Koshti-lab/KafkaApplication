0d68dfd7bec935625e1943739fa0e428e1baf60b
Himanshu Koshti <Himfeedback74@gmail.com>
2024-01-22 22:55:38 +0530

    first commit

---
+++ KafkaApplication/src/main/java/com/basic/demo/KafkaApplication.java
@@ -0,0 +1,13 @@
+package com.basic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDownloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDownloaderApplication.class, args);
+	}
+
+}
